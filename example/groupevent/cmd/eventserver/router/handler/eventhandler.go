543288c60f9aa9efa631083ee42ced051f21c56b
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-09 17:49:50 +0800

    implement new a event and invoke newaevent inside data

--- example/groupevent/cmd/eventserver/router/handler/eventhandler.go
+++ example/groupevent/cmd/eventserver/router/handler/eventhandler.go
@@ -1,12 +1,42 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"paper-code/example/groupevent/internal/eventserver/biz/event"
+)
+
+// How errors are handled:
+
+//Allowed to panic an application.
+//Wrap errors with context if not being handled.
+//Majority of handling errors happen here.
 
 type EventHandler struct {
 	BaseHandler
 }
 
 func (eventH *EventHandler) NewAEvent(rw http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		panic(errors.New("method request is mismatched"))
+	}
+
+	bodybytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	var dest event.Event
+	err = json.Unmarshal(bodybytes, &dest)
+	if err != nil {
+		panic(err)
+	}
+
+	err = event.NewAEvent(dest)
+	if err != nil {
+		panic(err)
+	}
 
 }
 
