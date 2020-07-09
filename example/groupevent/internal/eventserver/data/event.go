0d18f1246f4db18373cc051e5ae4a88f5cca51d0
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-09 18:44:26 +0800

    implement insert a event

--- example/groupevent/internal/eventserver/data/event.go
+++ example/groupevent/internal/eventserver/data/event.go
@@ -1,6 +1,14 @@
 package data
 
+import "paper-code/example/groupevent/internal/pkg/db"
+
+const insertaeventsql string = "INSERT INTO events(name,start_date,expired_on,member_count_limit,address,desc) VALUES (?,?,?,?,?,?)"
+
 func NewAEvent(name, startDate, expiredOn string, countLimit int, address, desc string) error {
+	_, err := db.NewDB().Exec(insertaeventsql, name, startDate, expiredOn, countLimit, address, desc)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
