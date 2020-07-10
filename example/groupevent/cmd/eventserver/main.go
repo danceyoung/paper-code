9ed531522f5c1feeaed47733b7fac1856f230a69
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-10 10:17:32 +0800

    log http server

--- example/groupevent/cmd/eventserver/main.go
+++ example/groupevent/cmd/eventserver/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	_ "paper-code/example/groupevent/cmd/eventserver/router"
+	_ "paper-code/example/groupevent/internal/pkg/cfg"
 )
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	log.Println(http.ListenAndServe(":8080", nil))
 }
