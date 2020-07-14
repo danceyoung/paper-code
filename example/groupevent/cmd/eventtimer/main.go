bc128ab8cbe7690ef2f8f6e11983a13ef97052df
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-14 17:26:58 +0800

    implement update event status by interval duration

--- example/groupevent/cmd/eventtimer/main.go
+++ example/groupevent/cmd/eventtimer/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"paper-code/example/groupevent/cmd/eventtimer/updater"
+	"time"
+)
+
+func main() {
+	//exec updating when 00:00:00 everyday
+	h, m, s := time.Now().Clock()
+	firstDuration, _ := time.ParseDuration(fmt.Sprintf("%dh%dm%ds", 23-h, 60-m, 60-s))
+	resetDuration, _ := time.ParseDuration(fmt.Sprintf("%dh%dm%ds", 24, 0, 0))
+	timer := time.NewTimer(firstDuration)
+	for {
+		select {
+		case <-timer.C:
+			timer.Reset(resetDuration)
+			log.Println("update: ", time.Now())
+			updater.UpdateEventStatus()
+		}
+	}
+}
