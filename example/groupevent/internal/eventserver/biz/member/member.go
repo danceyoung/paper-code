d0c7fc0c3bba6e96758c2d28f94dce6cc2a79d22
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-14 15:01:46 +0800

    implement biz logic for members

--- example/groupevent/internal/eventserver/biz/member/member.go
+++ example/groupevent/internal/eventserver/biz/member/member.go
@@ -1,5 +1,17 @@
 package member
 
-func membersBy(eventid string) {
+import (
+	"errors"
+	"log"
 
+	"paper-code/example/groupevent/internal/eventserver/data"
+)
+
+func MembersBy(eventid string) error {
+	if len(eventid) == 0 {
+		return errors.New("members by: params are not enough")
+	}
+	log.Println(data.MembersBy(eventid))
+
+	return nil
 }
