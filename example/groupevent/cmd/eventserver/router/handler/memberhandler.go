17c8158dd1ef9b59b5d9341c7a35339b7ddf7b0b
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-14 15:01:17 +0800

    implement member handler

--- example/groupevent/cmd/eventserver/router/handler/memberhandler.go
+++ example/groupevent/cmd/eventserver/router/handler/memberhandler.go
@@ -1,11 +1,16 @@
 package handler
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"paper-code/example/groupevent/internal/eventserver/biz/member"
+)
 
 type MemberHandler struct {
 	BaseHandler
 }
 
 func (mh *MemberHandler) Members(rw http.ResponseWriter, req *http.Request) {
-
+	log.Println(req.ParseForm())
+	member.MembersBy(req.Form.Get("event-id"))
 }
