bf17f7f7bb8202b09f5c0022e43b79e569217b09
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-14 15:26:35 +0800

    add responseWith func

--- example/groupevent/cmd/eventserver/router/handler/basehandler.go
+++ example/groupevent/cmd/eventserver/router/handler/basehandler.go
@@ -1,3 +1,33 @@
 package handler
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
 type BaseHandler struct{}
+
+func (bh *BaseHandler) responseWith(rw http.ResponseWriter, resbody interface{}, err error) {
+	if err != nil {
+		temp := make(map[string]interface{})
+		temp["code"] = -1
+		temp["msg"] = err.Error()
+		temp["data"] = nil
+		tempbytes, err := json.Marshal(temp)
+		if err != nil {
+			panic(err)
+		}
+		io.WriteString(rw, string(tempbytes))
+	} else {
+		temp := make(map[string]interface{})
+		temp["code"] = 0
+		temp["msg"] = ""
+		temp["data"] = resbody
+		tempbytes, err := json.Marshal(temp)
+		if err != nil {
+			panic(err)
+		}
+		io.WriteString(rw, string(tempbytes))
+	}
+}
