c8316d0a39d1497f36f30129c20d5470b617fc26
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-14 11:14:07 +0800

    implement insert a row for join a event

--- example/groupevent/internal/eventserver/data/event.go
+++ example/groupevent/internal/eventserver/data/event.go
@@ -3,6 +3,8 @@ package data
 import "paper-code/example/groupevent/internal/pkg/db"
 
 const insertaeventsql string = "INSERT INTO events(name,start_date,expired_on,member_count_limit,address,`desc`) VALUES (?,?,?,?,?,?)"
+const insertajoinsql string = `INSERT INTO event_members (event_id, student_name, student_id, g_m, college, level, profession) VALUES (?,?,?,?,?,?,?);`
+const selecteventsbystudentidsql string = "SELECT * FROM event_members where student_id=?"
 
 func NewAEvent(name, startDate, expiredOn string, countLimit int, address, desc string) error {
 	_, err := db.NewDB().Exec(insertaeventsql, name, startDate, expiredOn, countLimit, address, desc)
@@ -13,9 +15,19 @@ func NewAEvent(name, startDate, expiredOn string, countLimit int, address, desc 
 }
 
 func JoinAEvent(eventId string, name, gm, studentId, college, level, profession string) error {
+	_, err := db.NewDB().Exec(insertajoinsql, eventId, name, studentId, gm, college, level, profession)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func EventsBy(studentId string) {
+func EventsBy(studentId string) error {
+	rows, err := db.NewDB().Query(selecteventsbystudentidsql, studentId)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
+	return nil
 }
